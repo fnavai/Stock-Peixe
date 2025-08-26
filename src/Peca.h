9aaa142c43c2738d2d1c194b3ae5cfe8265bb97d
fnavai <felipenavai.contato@gmail.com>
2025-08-25 22:16:09 -0300

    Update Peca.h
    tipos de peças definidos

--- src/Peca.h
+++ src/Peca.h
@@ -1,1 +1,18 @@
+#ifndef PECA_H
+#define PECA_H
 
+#include <string>
+
+namespace xadrez{
+
+enum class TipoPeca{
+    NENHUMA = 0,
+    PEAO,
+    CAVALO,
+    BISPO,
+    TORRE,
+    RAINHA,
+    REI
+};
+
+}
